00cbddf5adb212a852e0cea3687958e69a11fe05
ThisIsLuckyluck <denislucas1a@gmail.com>
2025-04-02 17:08:02 +0200

    init project with vue framework

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import AppHeader from './components/Header.vue'
+</script>
+
+<template>
+  <AppHeader />
+</template>
+
+<style scoped>
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+}
+</style>
