cb1b7aa059b284f063e9424c22251fbb03117a95
ThisIsLuckyluck <denislucas1a@gmail.com>
2025-04-08 01:24:48 +0200

    adding router view and implementing Custom header for escape concat of header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
 import AppHeader from './components/Header.vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const hideHeaderOnRoutes = ['/Login', '/Register']
 </script>
 
 <template>
-  <AppHeader />
+  <AppHeader v-if="!hideHeaderOnRoutes.includes(route.path)" />
+  <router-view />
 </template>
 
 <style scoped>
